83af9209c1fe160067f409de92d4533769c642ca
Liam McGregor <liam@pop-os.localdomain>
2025-12-27 13:13:01 -0500

    modified:   Modelfile 	renamed:    dtl.xlsx -> go_dtl.xlsx 	modified:   main.py 	new file:   sm_team.xlsx 	new file:   sm_vhi.xlsx

--- main.py
+++ main.py
@@ -13,31 +13,51 @@ client = Client(
 Tk().withdraw()
 
 #  Fetch data files
-dtl_file = askopenfilename(title="Select DTL Excel file", filetypes=[("Excel files","*.xlsx")])
+go_dtl_file = askopenfilename(title="Select game-only DTL Excel file", filetypes=[("Excel files","*.xlsx")])
 freshness_file = askopenfilename(title="Select Freshness Excel file", filetypes=[("Excel files","*.xlsx")])
+sm_team_file = askopenfilename(title="Select SupraMax team file", filetypes=[("Excel files","*.xlsx")])
+sm_relative_file = askopenfilename(title="Select SupraMax relative file", filetypes=[("Excel files","*.xlsx")])
 
 #  Create dataframes
-df1 = pd.read_excel(dtl_file)
+df1 = pd.read_excel(go_dtl_file)
 df2 = pd.read_excel(freshness_file)
+df3 = pd.read_excel(sm_team_file)
+df4 = pd.read_excel(sm_relative_file)
 
 #  Convert xlsx to text
-dtl_text = df1.to_string(index=False)
+go_dtl_text = df1.to_string(index=False)
 freshness_text = df2.to_string(index=False)
+sm_team_text = df3.to_string(index=False)
+sm_relative_text = df4.to_string(index=False)
 
 #  Print text data for user
-print(f'**************************************************************\n{dtl_text}\n\n{freshness_text}\n**************************************************************')
+#print(f'**************************************************************\n{go_dtl_text}\n\n{freshness_text}\n**************************************************************')
+
 
 #  Build and send prompt to Ollama
 response: ChatResponse = client.chat(model='ministral-data-analyst:latest', messages=[
     {
         #  for system role tweaks, use modelfile
         'role': 'user',
-        'content': f'Here are the DTL scores:\n{dtl_text}'
+        'content': f'Here are the game-only DTL scores:\n{go_dtl_text}'
     },
     {
         'role': 'user',
-        'content': f'Here are the freshness scores:\n{freshness_text}'
+        'content': f'Here are the team total SupraMax efforts:\n{sm_team_text}'
+    },
+    {
+        'role': 'user',
+        'content': f'Here are the player personal average(VHI) SupraMax efforts:\n{sm_relative_text}'
     }
     ])
 
 print(response.message.content)
+
+EXIT = input('Press ENTER to exit')
+
+'''
+{
+        'role': 'user',
+        'content': f'Here are the freshness scores:\n{freshness_text}'
+    },
+'''
